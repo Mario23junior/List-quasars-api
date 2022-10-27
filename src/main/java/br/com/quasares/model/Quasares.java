ea894864242839db5d4ea92c54d3dd141e4f8b12
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-10-27 15:18:43 -0300

    Criando model do banco de dados.

---
+++ src/main/java/br/com/quasares/model/Quasares.java
@@ -0,0 +1,99 @@
+package br.com.quasares.model;
+
+public class Quasares {
+
+	private Long id;
+	private String nome;
+	private String img;
+	private String ascensaoDireita;
+	private String declinacao;
+	private String nomeConstalacao;
+	private String desvioVermelho;
+	private String tipo;
+	private String distancia;
+	private String magnetudeAparente;
+
+	public Quasares() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getImg() {
+		return img;
+	}
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+
+	public String getAscensaoDireita() {
+		return ascensaoDireita;
+	}
+
+	public void setAscensaoDireita(String ascensaoDireita) {
+		this.ascensaoDireita = ascensaoDireita;
+	}
+
+	public String getDeclinacao() {
+		return declinacao;
+	}
+
+	public void setDeclinacao(String declinacao) {
+		this.declinacao = declinacao;
+	}
+
+	public String getNomeConstalacao() {
+		return nomeConstalacao;
+	}
+
+	public void setNomeConstalacao(String nomeConstalacao) {
+		this.nomeConstalacao = nomeConstalacao;
+	}
+
+	public String getDesvioVermelho() {
+		return desvioVermelho;
+	}
+
+	public void setDesvioVermelho(String desvioVermelho) {
+		this.desvioVermelho = desvioVermelho;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getDistancia() {
+		return distancia;
+	}
+
+	public void setDistancia(String distancia) {
+		this.distancia = distancia;
+	}
+
+	public String getMagnetudeAparente() {
+		return magnetudeAparente;
+	}
+
+	public void setMagnetudeAparente(String magnetudeAparente) {
+		this.magnetudeAparente = magnetudeAparente;
+	}
+}
